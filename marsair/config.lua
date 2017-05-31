894c33e3f407df9871e93cbf7417381651ed66e3
cpdef <cpdef@protonmail.com>
2017-05-31 23:29:18 +0200

    initial

---
+++ marsair/config.lua
@@ -0,0 +1,16 @@
+marsairconfig.leak_time = 30*60
+marsairconfig.leak_chance = 10
+
+marsairconfig.need_air_generation_count = 8
+marsairconfig.need_air_fixleak_count = 4
+
+marsairconfig.flower_air_time = 20*60
+marsairconfig.flower_air_chance = 4
+
+marsairconfig.tree_air_time = 40*60
+marsairconfig.tree_air_chance = 10
+marsairconfig.tree_air_disappear_time = 10
+marsairconfig.tree_air_maker_time = 8
+
+marsairconfig.vacuum_leak_speed = 8
+marsairconfig.vacuum_leak_chance = 5
